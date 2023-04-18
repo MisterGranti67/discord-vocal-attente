ef0f5dfeca501f58291f98405258311d700cb102
Gauthier Corion <43696669+MisterGranti67@users.noreply.github.com>
2023-04-18 15:00:35 +0200

    fix(Join): Fix join and more features

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const { createAudioPlayer, createAudioResource, entersState, joinVoiceChannel, A
 const { Client, Intents } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
-
+const { generateDependencyReport } = require('@discordjs/voice');
+console.log(generateDependencyReport());
 const client = new Client({
   intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]
 });
 
 const targetVoiceChannelId = '589102132817559553';
+const guildmessage = '1084535636222017546';
 const targetGuildId = '451372360134819860';
 const player = createAudioPlayer();
 
@@ -27,28 +29,27 @@ client.on('messageCreate', async (message) => {
     }
 });
 client.on('voiceStateUpdate', async (oldState, newState) => {
+    const targetGuild = client.guilds.cache.get(guildmessage);
     if (newState.channelId === targetVoiceChannelId && newState.member.id !== client.user.id) {
-        const username = newState.member.displayName;
+        const userId = newState.member.id;
 
-        const targetTextChannel = newState.guild.channels.cache.get('1086413403767181394');
+        const targetTextChannel = targetGuild.channels.cache.get('1086413403767181394');
 
         if (!targetTextChannel || targetTextChannel.type !== 'GUILD_TEXT') return;
 
-        if (currentTrack === 'musique_attente_1.mp3') {
-            targetTextChannel.send(`Machin est dans attente d'aide !`);
-        } else if (currentTrack === 'sons_attente.mp3') {
-            targetTextChannel.send(`@Lody est dans attente d'aide ! @New.Guide-SL @Guide-SL`);
+        if (currentTrack === 'sons_attente.mp3') {
+            targetTextChannel.send(`<@${userId}> est dans attente d'aide !`);
+        } else if (currentTrack === 'musique_attente_1.mp3') {
+            targetTextChannel.send(`<@${userId}> est dans attente d'aide ! <@&1084556184725508147> <@&1084555956995756137>`);
         }
     }
 });
 
 const playAudio = async (guild, filePath) => {
     const targetGuild = client.guilds.cache.get(targetGuildId);
-    console.log("1"+targetGuild)
     if (!targetGuild) return;
     
     const voiceChannel = targetGuild.channels.cache.get(targetVoiceChannelId);
-    console.log(voiceChannel)
     if (!voiceChannel || voiceChannel.type !== 'GUILD_VOICE') return;
 
     const connection = joinVoiceChannel({
@@ -58,14 +59,18 @@ const playAudio = async (guild, filePath) => {
     });
 
     try {
+        console.log('Trying to join voice channel...')
         await entersState(connection, VoiceConnectionStatus.Ready, 30_000);
     } catch (error) {
         console.error(error);
         connection.destroy();
+        console.log('Failed to join voice channel within 30 seconds, destroying the connection.')
         return;
     }
 
+    console.log("Playing audio file: " + filePath);
     const resource = createAudioResource(fs.createReadStream(filePath), {
+        
         inputType: StreamType.Arbitrary,
     });
 
@@ -73,6 +78,7 @@ const playAudio = async (guild, filePath) => {
     connection.subscribe(player);
 
     player.on(AudioPlayerStatus.Idle, () => {
+        console.log('Audio player is idle, destroying the connection.')
         playAudio(guild, filePath);
     });
 };

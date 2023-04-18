e5c66a6d6ae4cb2aa5cf48ddb27b973c884e9173
mrbaguette07 <mathsgovlog@gmail.com>
2023-04-18 14:30:02 +0200

    fix(tokeen): Fix token & join vocal

--- index.js
+++ index.js
@@ -1,75 +1,85 @@
+const { createAudioPlayer, createAudioResource, entersState, joinVoiceChannel, AudioPlayerStatus, VoiceConnectionStatus, StreamType } = require('@discordjs/voice');
 const { Client, Intents } = require('discord.js');
-const { createAudioResource, createAudioPlayer, joinVoiceChannel, AudioPlayerStatus, StreamType } = require('@discordjs/voice');
 const fs = require('fs');
+require('dotenv').config();
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]
 });
 
-const token = 'NDgwODA3NzY5MzM1NzkxNjI2.GrX9_1.yroNtNosnp2wfggbIB_ILfjWPa8Eo0fIK1yD6Y';
 const targetVoiceChannelId = '589102132817559553';
+const targetGuildId = '451372360134819860';
+const player = createAudioPlayer();
 
-let audioPlayer = createAudioPlayer();
-let currentConnection;
-let currentTrack;
+let currentTrack = null;
+let currentConnection = null;
 
-const playAudio = (channel, filePath) => {
-  if (currentConnection) currentConnection.destroy();
-
-  currentConnection = joinVoiceChannel({
-    channelId: channel.id,
-    guildId: channel.guild.id,
-    adapterCreator: channel.guild.voiceAdapterCreator
-  });
-
-  audioPlayer.stop();
-  audioPlayer = createAudioPlayer();
-
-  const audioResource = createAudioResource(fs.createReadStream(filePath), {
-    inputType: StreamType.Arbitrary
-  });
-
-  audioPlayer.play(audioResource);
-  currentConnection.subscribe(audioPlayer);
-
-  audioPlayer.on(AudioPlayerStatus.Idle, () => {
-    currentConnection.destroy();
-  });
-
-  audioPlayer.on('error', (error) => {
-    console.error('Error playing audio:', error);
-    currentConnection.destroy();
-  });
-};
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
 
 client.on('messageCreate', async (message) => {
-  if (!message.guild) return; // Ignore DMs
+    if (!message.guild || message.channel.type !== 'GUILD_TEXT') return;
 
-  const voiceChannel = message.guild.channels.cache.get(targetVoiceChannelId);
-  if (!voiceChannel || voiceChannel.type !== 'GUILD_VOICE') return; // Ignore if target channel is not found or not a voice channel
-
-  if (message.content === '-attentelody on') {
-    currentTrack = 'musique_attente_1.mp3';
-    playAudio(voiceChannel, currentTrack);
-  } else if (message.content === '-attentelody off') {
-    currentTrack = 'sons_attente.mp3';
-    playAudio(voiceChannel, currentTrack);
-  }
-});
-
-client.on('voiceStateUpdate', (oldState, newState) => {
-  if (newState.channelId === targetVoiceChannelId && newState.member.id !== client.user.id) {
-    const username = newState.member.displayName;
-    if (currentTrack === 'musique_attente_1.mp3') {
-      newState.guild.systemChannel.send(`Machin est dans attente d'aide !`);
-    } else if (currentTrack === 'sons_attente.mp3') {
-      newState.guild.systemChannel.send(`@Lody est dans attente d'aide ! @New.Guide-SL @Guide-SL`);
+    if (message.content === '-attentelody on') {
+        currentTrack = 'musique_attente_1.mp3';
+        playAudio(message.guild, currentTrack);
+    } else if (message.content === '-attentelody off') {
+        currentTrack = 'sons_attente.mp3';
+        playAudio(message.guild, currentTrack);
     }
-  }
+});
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    if (newState.channelId === targetVoiceChannelId && newState.member.id !== client.user.id) {
+        const username = newState.member.displayName;
+
+        const targetTextChannel = newState.guild.channels.cache.get('1086413403767181394');
+
+        if (!targetTextChannel || targetTextChannel.type !== 'GUILD_TEXT') return;
+
+        if (currentTrack === 'musique_attente_1.mp3') {
+            targetTextChannel.send(`Machin est dans attente d'aide !`);
+        } else if (currentTrack === 'sons_attente.mp3') {
+            targetTextChannel.send(`@Lody est dans attente d'aide ! @New.Guide-SL @Guide-SL`);
+        }
+    }
 });
 
-client.login(token);
+const playAudio = async (guild, filePath) => {
+    const targetGuild = client.guilds.cache.get(targetGuildId);
+    console.log("1"+targetGuild)
+    if (!targetGuild) return;
+    
+    const voiceChannel = targetGuild.channels.cache.get(targetVoiceChannelId);
+    console.log(voiceChannel)
+    if (!voiceChannel || voiceChannel.type !== 'GUILD_VOICE') return;
+
+    const connection = joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: voiceChannel.guild.id,
+        adapterCreator: voiceChannel.guild.voiceAdapterCreator
+    });
+
+    try {
+        await entersState(connection, VoiceConnectionStatus.Ready, 30_000);
+    } catch (error) {
+        console.error(error);
+        connection.destroy();
+        return;
+    }
+
+    const resource = createAudioResource(fs.createReadStream(filePath), {
+        inputType: StreamType.Arbitrary,
+    });
+
+    player.play(resource);
+    connection.subscribe(player);
+
+    player.on(AudioPlayerStatus.Idle, () => {
+        playAudio(guild, filePath);
+    });
+};
+  
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.BOT_TOKEN);
